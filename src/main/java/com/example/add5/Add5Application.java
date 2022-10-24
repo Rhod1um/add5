53ef3753a80541fda6a4e9c1b0ef385457314f80
Veronica <frida-nanni@hotmail.com>
2022-10-24 19:58:23 +0200

    first commit

---
+++ src/main/java/com/example/add5/Add5Application.java
@@ -0,0 +1,15 @@
+package com.example.add5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Add5Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Add5Application.class, args);
+  }
+  //hvorfor laver han klasser med små bogstaver?
+  //hvorfor bliver templates og Home ét frem for at Home
+  //grafisk ligger under templates? Nu kan man ikke lægge noget under templates, kun Home
+}
